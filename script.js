dbf991b7cb6a8e0e9c8c8098b01be15ed5c27e3a
Awsal98 <awsalaladwani98@gmail.com>
2022-12-07 19:53:06 +0300

    done

---
+++ script.js
@@ -0,0 +1,16 @@
+alert ("welcome to my bage")
+let grade= prompt('enter your grade')
+console.log(grade)
+if(grade>=90 && grade<=100)
+{console.log("%c your grade is execllent")}
+else if (grade>=80 && grade<=89)
+{console.log( "your grade is very good")}
+else if(grade>=70 && grade<=79)
+{console.log( "your grade is good")}
+else if(grade>=60 && grade<=69)
+{console.log( "your grade is fairly good")}
+else if(grade>=50 && grade<=59)
+{console.log( "your grade is F")}
+else if(grade<50)
+{console.log( "your grade is fail")}
+// javascript list java
